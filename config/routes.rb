065e33ff178b93fb555838a4248bc454508d12da
Dominic White <dominicpaulwhite@gmail.com>
2015-05-24 19:36:18 +1000

    added sessions_controller and omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get '/about' => 'pages#about'
 
+  
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#auth_fail'
+  get '/sign_out', to: 'sessions#destroy', as: :sign_out
+
 end
