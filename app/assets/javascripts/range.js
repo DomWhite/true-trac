413d361cf2efd62f174a83878a53ad305e29efd3
Dominic White <dominicpaulwhite@gmail.com>
2015-05-29 10:36:19 +1000

    css changes

--- app/assets/javascripts/range.js
+++ app/assets/javascripts/range.js
@@ -14,3 +14,26 @@ function setRange() {
 $(function(){
   setRange();
 })
+
+var ammo = {
+  sg12: 150,
+  r22: 300,
+  r308: 800,
+  bmg50:3000
+}
+
+function drawCircle(ammo) {
+      var center = new google.maps.LatLng(crd.latitude, crd.longitude);
+          var options = {
+            strokeColor: 'red',
+            strokeOpacity: 1.0,
+            strokeWeight: 1,
+            fillColor: 'red',
+            fillOpacity: 0.5,
+            map: map,
+            center: center,
+            radius: ammo
+        };
+
+        circle = new google.maps.Circle(options);
+}
